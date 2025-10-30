cead1cacac17b6b46d225b873a1a6452150e99c4
WENJIA CHEN <ryokaa77@gmail.com>
2025-10-30 20:19:51 +0800

    update files and add test file

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('<!doctype html>\n<html>\n<head><title>Express</title></head>\n<body>\n  <h1>Express</h1>\n  <p>My Student ID is 21849349.</p>\n  <p>Welcome to Express</p>\n</body>\n</html>');
+  res.render('index', { title: 'World' });
 });
-
 export default router;
